df73692c47b9848fae1adb0b057040dd18907ce5
Hamza zahid <138646003+hamzazahi@users.noreply.github.com>
2023-07-05 09:57:08 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,368 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(" Hello Pakistan");
+            /* Multiline commit in C# */
+
+            Console.WriteLine("This the console based e-portal ");
+
+
+            Console.WriteLine("Enter user id");
+            int accn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter user password");
+            int accpp = Convert.ToInt32(Console.ReadLine());
+            if (accn == 8556205 && accpp == 855)
+            {
+                Console.WriteLine("Welcome to the e-portal sir");
+            }
+            else
+            {
+                Console.WriteLine("Wrong user or password sir");
+            }
+            Console.WriteLine("*********************");
+            Console.WriteLine("Select 1 for Transfer");
+            Console.WriteLine("Select 2 for Paybill");
+            Console.WriteLine("Select 3 for Raast id Management");
+            Console.WriteLine("Select 4 for Education payment");
+            
+
+            Console.WriteLine("select your choice please for move forward");
+            int abc = Convert.ToInt32(Console.ReadLine());
+            switch (abc)
+            {
+                case 1:
+                    Console.WriteLine("1: Existing Payees ");
+                    Console.WriteLine("2: Instant transfer ");
+
+                    Console.WriteLine("select your choice please for move forward");
+                    int abd = Convert.ToInt32(Console.ReadLine());
+                    switch (abd)
+                    {
+                        case 1:
+                            Console.WriteLine("Please Select the account to transfer ");
+                            Console.WriteLine("1:Hamza Zahid ");
+                            Console.WriteLine("2:Asif Baloch ");
+                            Console.WriteLine("3:Abid baloch ");
+                            Console.WriteLine("4:Ali Baloch ");
+                            Console.WriteLine("select your choice please for move forward");
+                            int acc = Convert.ToInt32(Console.ReadLine());
+                            switch (acc)
+                            {
+                                case 1:
+                                    Console.WriteLine("Transfer Form *0000************78");
+                                    Console.WriteLine("Please enter the amount");
+                                    int aa = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(aa);
+                                    Console.WriteLine("Select Purpose");
+                                    Console.WriteLine("1 Family support");
+                                    Console.WriteLine("2 Donation");
+                                    Console.WriteLine("3 fee payment");
+                                    Console.WriteLine("4 Salary payment");
+                                    Console.WriteLine("Please enter Purpose index");
+                                    int bb = Convert.ToInt32(Console.ReadLine());
+                                    switch (bb)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+
+                                    }
+
+
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Transfer Form *0000************78");
+                                    Console.WriteLine("Please enter the amount");
+                                    int aas = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(aas);
+                                    Console.WriteLine("Select Purpose");
+                                    Console.WriteLine("1 Family support");
+                                    Console.WriteLine("2 Donation");
+                                    Console.WriteLine("3 fee payment");
+                                    Console.WriteLine("4 Salary payment");
+                                    Console.WriteLine("Please enter Purpose index");
+                                    int bbs = Convert.ToInt32(Console.ReadLine());
+                                    switch (bbs)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Done SIR");
+                                            break;
+                                    }
+
+                                    break;
+
+                                case 3:
+                                    Console.WriteLine("Transfer Form *0000************78");
+                                    Console.WriteLine("Please enter the amount");
+                                    int aab = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(aab);
+                                    Console.WriteLine("Select Purpose");
+                                    Console.WriteLine("1 Family support");
+                                    Console.WriteLine("2 Donation");
+                                    Console.WriteLine("3 fee payment");
+                                    Console.WriteLine("4 Salary payment");
+                                    Console.WriteLine("Please enter Purpose index");
+                                    int bbb = Convert.ToInt32(Console.ReadLine());
+                                    switch (bbb)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+
+                                    }
+
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Transfer Form *0000************78");
+                                    Console.WriteLine("Please enter the amount");
+                                    int aac = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(aac);
+                                    Console.WriteLine("Select Purpose");
+                                    Console.WriteLine("1 Family support");
+                                    Console.WriteLine("2 Donation");
+                                    Console.WriteLine("3 fee payment");
+                                    Console.WriteLine("4 Salary payment");
+                                    Console.WriteLine("Please enter Purpose index");
+                                    int bbc = Convert.ToInt32(Console.ReadLine());
+                                    switch (bbc)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                    }
+
+                                    break;
+
+
+                            }
+
+
+                            break;
+                        case 2:
+                            Console.WriteLine("1 With in same the bank");
+                            Console.WriteLine("2 Other bank");
+
+                            Console.WriteLine("please select catagory");
+                            int bbx = Convert.ToInt32(Console.ReadLine());
+                            switch (bbx)
+                            {
+                                case 1:
+                                    Console.WriteLine("Please enter the account number");
+                                    int aa = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(aa);
+                                    Console.WriteLine("Transfer Form *0000************78");
+                                    Console.WriteLine("Please enter the amount");
+                                    int cc = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(cc);
+                                    Console.WriteLine("Select Purpose");
+                                    Console.WriteLine("1 Family support");
+                                    Console.WriteLine("2 Donation");
+                                    Console.WriteLine("3 fee payment");
+                                    Console.WriteLine("4 Salary payment");
+                                    Console.WriteLine("Please enter Purpose index");
+                                    int xx = Convert.ToInt32(Console.ReadLine());
+                                    switch (xx)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Please Proceed");
+                                            break;
+                                    }
+
+
+                                    break;
+                                case 2:
+                                    Console.WriteLine(" Select the bank category");
+                                    int ll = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine("1 Advance Microfinance ");
+                                    Console.WriteLine("2 Allied Bank Limited");
+                                    Console.WriteLine("3 Askari Bank Limited ");
+                                    Console.WriteLine("4 Bank Al-Habib Limited");
+                                    Console.WriteLine("5 Habib bank Limited");
+
+                                    Console.WriteLine("Please Select the bank catagory");
+                                    int ss = Convert.ToInt32(Console.ReadLine());
+                                    break;
+                            }
+
+
+                            break;
+                    }
+
+
+
+                    break;
+                case 2:
+                    Console.WriteLine("Please Select the biller Method ");
+                    Console.WriteLine("1 Saved Biller ");
+                    Console.WriteLine("2 Add New Biller");
+                    Console.WriteLine("Enter the index");
+                    int pp = Convert.ToInt32(Console.ReadLine());
+                    switch (pp)
+                    {
+                        case 1:
+                            Console.WriteLine("Biller Name");
+                            string kk = Console.ReadLine();
+                            Console.WriteLine("*******8525628*******");
+                            Console.WriteLine("Your bill amount is Rs 89520 ");
+                            break;
+                        case 2:
+                            Console.WriteLine("Please Select your Biller Category");
+                            int nn = Convert.ToInt32(Console.ReadLine());
+
+                            switch (nn)
+                            {
+                                case 1:
+                                    Console.WriteLine("1 1-Bill");
+                                    Console.WriteLine("2 Agregator");
+                                    Console.WriteLine("3  Clubs");
+                                    Console.WriteLine("4 DHA");
+                                    Console.WriteLine("5 Electricity");
+
+                                    Console.WriteLine("Please Select your Biller Category");
+                                    int mm = Convert.ToInt32(Console.ReadLine());
+                                    switch (mm)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Enter Accout number of 1 bill ");
+                                            int oo = Convert.ToInt32(Console.ReadLine());
+                                            Console.WriteLine("Your bill is added");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Enter Accout number of Agregator ");
+                                            int oos = Convert.ToInt32(Console.ReadLine());
+                                            Console.WriteLine("Your bill is added");
+
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("Enter Accout number of Club bill ");
+                                            int oon = Convert.ToInt32(Console.ReadLine());
+                                            Console.WriteLine("Your bill is added");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Enter Accout number of Electricity bill ");
+                                            int ook = Convert.ToInt32(Console.ReadLine());
+                                            Console.WriteLine("Your bill is added");
+                                            break;
+                                    }
+
+
+
+                                    break;
+                            }
+
+                            break;
+
+
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Welcome for Raast id Management");
+                    Console.WriteLine("1 For Registration ");
+                    Console.WriteLine("2 For Send money Through Raast ");
+                    Console.WriteLine("Select your choice ");
+                    int k = Convert.ToInt32(Console.ReadLine());
+                    switch (k)
+                    {
+                       case 1:
+                      Console.WriteLine("Enter Your iban number");
+                            int p = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Your salutation");
+                            string z = Console.ReadLine();
+                            Console.WriteLine("Enter Rast id type");
+                            string u = Console.ReadLine();
+
+                            break;
+                        case 2 :
+                            Console.WriteLine("Enter sender iban number");
+                            int yp = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter their salutation");
+                            string zs = Console.ReadLine();
+                            Console.WriteLine("Enter Rast id number");
+                            int ks = Convert.ToInt32(Console.ReadLine());
+
+                            break;
+
+                    }
+           
+                    break;
+                case 4:
+                    Console.WriteLine("Education Payment");
+                    Console.WriteLine("Enter university account number");
+                    int ypo = Convert.ToInt32(Console.ReadLine());
+                    if(ypo==1234)
+                    {
+                        Console.WriteLine("Pafkiet");
+                        Console.WriteLine("fees status done");
+                    }
+                    else if(ypo==5678)
+                    {
+                        Console.WriteLine("Pafkiet City campus");
+                        Console.WriteLine("fees status done");
+                    }
+
+                    else if (ypo == 91011)
+                    {
+                        Console.WriteLine("Pafkiet north campus");
+                        Console.WriteLine("fees status done");
+                    }
+
+
+                    break;
+                
+            }
+
+
+
+        }
+    }
+}
